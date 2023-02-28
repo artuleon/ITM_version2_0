8d1dca5363d9d801529087408f77c11991c973fb
Arturo S. Leon <artuleon@gmail.com>
2023-02-28 09:23:28 -0500

    fixing .gitignore

--- engine/build/ITM_Debug/endprog__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Feb 27 23:29:49 2023
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE ENDPROG__genmod
-          INTERFACE 
-            SUBROUTINE ENDPROG
-            END SUBROUTINE ENDPROG
-          END INTERFACE 
-        END MODULE ENDPROG__genmod
--- engine/build/ITM_Debug/timestamp__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Feb 27 23:29:46 2023
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE TIMESTAMP__genmod
-          INTERFACE 
-            SUBROUTINE TIMESTAMP
-            END SUBROUTINE TIMESTAMP
-          END INTERFACE 
-        END MODULE TIMESTAMP__genmod
